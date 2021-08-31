3001a60fd83e2d4903180c470263412939ab2d2d
mindolink <ice.cvicek@gmail.com>
2021-09-01 01:28:01 +0200

    lepo

--- SimulatorUser/homeStorageBattery.py
+++ SimulatorUser/homeStorageBattery.py
@@ -111,7 +111,7 @@ class homeStorageBattery(object):
         if (self.WeekNumber<6):
             if (self.SOC<self.SOCsmart and (self.Hour<6 or 21<self.Hour)):
                 dtWb=(self.SOCsmart-self.SOC)*self.Wb
-                P=dtWb/(self.TarInt*0.65)
+                P=dtWb/(self.TarInt*0.75)
                 if P<self.PbCh:
                     self.PbAvSr=0
                     self.PbAvLd=self.PbCh-P
